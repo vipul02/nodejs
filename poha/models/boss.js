cbdc6911e6f69cada9ce8a017328a9659c8dc4ce
vipul02 <lalwanivipul2@gmail.com>
2018-08-01 11:06:34 +0530

    mongoose, its schemas and models

---
+++ poha/models/boss.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+// create custom schema for poha boses
+const BossSchema = new Schema({
+    name: {
+        type: String,
+        required:[true, 'Name field is required']
+    },
+    specs: {
+        type: String
+    },
+    availability: {
+        type: Boolean,
+        default: false
+    }
+    // add a geo location
+});
+
+const Boss = mongoose.model('boss', BossSchema);
+
+module.exports = Boss;
