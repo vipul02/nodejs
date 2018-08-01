82be3b66d91a57ceba944e8db4382faecd513f17
vipul02 <lalwanivipul2@gmail.com>
2018-08-01 08:04:19 +0530

    Handling get request

--- poha/index.js
+++ poha/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 // set up express app
 const app = express();
 
+// Handle requests
+app.get('/api', function (req, res) {
+    console.log('GET request');
+    res.send({ name: 'Bhavy' });
+});
+
 // listen to request
 app.listen(4000, function () {
    console.log('Listening to the request at port no. 4000')
